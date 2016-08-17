0864c86c446f3fbdeb3abb64eab60cf5ccdfaa6c
Michael Chen <mzc2fd@virginia.edu>
2016-08-17 10:33:05 -0400

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name templateAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the templateAppApp
+ */
+angular.module('templateAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
